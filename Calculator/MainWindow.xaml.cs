bc4d3e8fb0ac8eae923f1e6953abb7e955ba4f77
tekinosmanbaran@gmail.com <tekinosmanbaran@gmail.com>
2024-07-16 15:48:17 +0300

    enum kullanımı, (sender as Button) kullanımı, Static resource kullanımı öğrenildi

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -21,123 +21,95 @@ namespace Calculator
     public partial class MainWindow : Window
     {
         double lastNumber, result;
-
+        SelectedOperator selectedOperator;
+        
         public MainWindow()
         {
             InitializeComponent();
-            buttonAC.Click += ButtonAC_Click;
-            buttonNegative.Click += ButtonNegative_Click;
-            buttonPercentage.Click += ButtonPercentage_Click;
-            buttonDivide.Click += ButtonDivide_Click;
-            buttonSeven.Click += ButtonSeven_Click;
-            buttonEight.Click += ButtonEight_Click;
-            buttonNine.Click += ButtonNine_Click;
-            buttonMultiply.Click += ButtonMultiply_Click;
-            buttonFour.Click += ButtonFour_Click;
-            buttonFive.Click += ButtonFive_Click;
-            buttonSix.Click += ButtonSix_Click;
-            buttonMinus.Click += ButtonMinus_Click;
-            buttonOne.Click += ButtonOne_Click;
-            buttonTwo.Click += ButtonTwo_Click;
-            buttonThree.Click += ButtonThree_Click;
-            buttonPlus.Click += ButtonPlus_Click;
-            buttonZero.Click += ButtonZero_Click;
-            buttonDot.Click += ButtonDot_Click;
-            buttonEqual.Click += ButtonEqual_Click;
-
-
         }
 
-        private void ButtonEqual_Click(object sender, RoutedEventArgs e)
+        private void ButtonDot_Click ( object sender, RoutedEventArgs e )
         {
-            throw new NotImplementedException();
+            if ( resultLabel.Content.ToString().Contains( "." ) )
+            {
+                // Do nothing
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}.";
+            }
         }
 
-        private void ButtonDot_Click(object sender, RoutedEventArgs e)
+        private void ButtonEqual_Click ( object sender, RoutedEventArgs e )
         {
-            throw new NotImplementedException();
+            double newNumber;
+            if ( double.TryParse( resultLabel.Content.ToString(), out newNumber ) )
+            {
+                switch ( selectedOperator )
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber,newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber,newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber,newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = SimpleMath.Substract(lastNumber,newNumber);
+                        break;
+                }
+            }
+            resultLabel.Content = result.ToString();
         }
 
-        private void ButtonZero_Click(object sender, RoutedEventArgs e)
+        private void ButtonNumber_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            int selectedValue = int.Parse(( sender as Button ).Content.ToString());
+
+            if ( resultLabel.Content.ToString() == "0" )
+            {
+                resultLabel.Content = $"{selectedValue}";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}{selectedValue}";
+            }
         }
 
-        private void ButtonPlus_Click(object sender, RoutedEventArgs e)
+        private void OperationButton_Click ( object sender, RoutedEventArgs e )
         {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonThree_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonTwo_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonOne_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonMinus_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonSix_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonFive_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonFour_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonMultiply_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonNine_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonEight_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonSeven_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void ButtonDivide_Click(object sender, RoutedEventArgs e)
-        {
-            throw new NotImplementedException();
+            if ( double.TryParse( resultLabel.Content.ToString(), out lastNumber ) )
+            {
+                resultLabel.Content = "0";
+            }
+            if ( sender == buttonMultiply )
+                selectedOperator = SelectedOperator.Multiplication;
+            else if ( sender == buttonDivide )
+                selectedOperator = SelectedOperator.Division;
+            else if ( sender == buttonPlus )
+                selectedOperator = SelectedOperator.Addition;
+            else if ( sender == buttonMinus )
+                selectedOperator = SelectedOperator.Substraction;
         }
 
         private void ButtonPercentage_Click(object sender, RoutedEventArgs e)
         {
-            throw new NotImplementedException();
+            if ( double.TryParse( resultLabel.Content.ToString(), out lastNumber ) )
+            {
+                if(lastNumber != 0)
+                    lastNumber /= 100;
+                resultLabel.Content = lastNumber.ToString();
+            }
         }
 
         private void ButtonNegative_Click(object sender, RoutedEventArgs e)
         {
             if (double.TryParse(resultLabel.Content.ToString(), out lastNumber)) 
             {
-                lastNumber *= -1;
+                if(lastNumber != 0)
+                    lastNumber *= -1;
                 resultLabel.Content = lastNumber.ToString();
             }
         }
@@ -147,4 +119,13 @@ namespace Calculator
             resultLabel.Content = "0";
         }
     }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
 }
---
+++ Calculator/SimpleMath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Calculator
+{
+    public class SimpleMath
+    {
+
+        public static double Add ( double x, double y )
+        {
+            return x + y;
+        }
+
+        public static double Substract ( double x, double y )
+        {
+            return x - y;
+        }
+
+        public static double Multiply ( double x, double y )
+        {
+            return x * y;
+        }
+
+        public static double Divide ( double x, double y )
+        {
+            if ( y == 0 )
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+
+            return x / y;
+        }
+    }
+}
